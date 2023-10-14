a744d1f50f8490cb1cd314577a587b20e1201f5a
Harshit Gupta <98156928+Harshitg551@users.noreply.github.com>
2023-10-14 18:05:13 +0530

    Create TicTacToe.py

---
+++ TicTacToe.py
@@ -0,0 +1,64 @@
+# Function to print the Tic Tac Toe board
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("---------")
+
+# Function to check if a player has won
+def check_winner(board, player):
+    # Check rows
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+    # Check columns
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+
+    # Check diagonals
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+
+    return False
+
+# Function to check if the board is full (a draw)
+def is_board_full(board):
+    for row in board:
+        if " " in row:
+            return False
+    return True
+
+# Function to play the game
+def play_game():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = "X"
+
+    while True:
+        print_board(board)
+        print(f"Player {current_player}'s turn.")
+        row = int(input("Enter row (0, 1, or 2): "))
+        col = int(input("Enter column (0, 1, or 2): "))
+
+        if board[row][col] != " ":
+            print("Cell already taken. Try again.")
+            continue
+
+        board[row][col] = current_player
+
+        if check_winner(board, current_player):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+
+        if is_board_full(board):
+            print_board(board)
+            print("It's a draw!")
+            break
+
+        # Switch to the other player for the next turn
+        current_player = "O" if current_player == "X" else "X"
+
+# Main entry point to start the game
+if __name__ == "__main__":
+    play_game()
